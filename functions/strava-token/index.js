80fe2ccd57dceac6f716b97a97c84486a486c027
benbartz1129-hue <benbartz1129@gmail.com>
2026-06-12 10:11:26 -0500

    Create index.js

---
+++ functions/strava-token/index.js
@@ -0,0 +1,38 @@
+export async function onRequestPost(context) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    const body = await context.request.json();
+
+    const params = new URLSearchParams();
+    params.append('client_id', '256087');
+    params.append('client_secret', context.env.STRAVA_CLIENT_SECRET || '');
+    params.append('grant_type', body.grant_type);
+    if (body.code) params.append('code', body.code);
+    if (body.refresh_token) params.append('refresh_token', body.refresh_token);
+
+    const r = await fetch('https://www.strava.com/oauth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: params
+    });
+
+    const text = await r.text();
+    return new Response(text, { status: r.status, headers });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: e.message }), { status: 500, headers });
+  }
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    }
+  });
+}
